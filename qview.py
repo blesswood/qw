8cf6a91c08d68887bd5f838772d9dbb7f52da7cf
blesswood <40756812+blesswood@users.noreply.github.com>
2021-05-25 10:25:40 +0300

    Add delete and list args

--- qview.py
+++ qview.py
@@ -15,25 +15,45 @@ if ("-h" in sys.argv or len(sys.argv)<=1 or "-h" in sys.argv):
 
 """)
     print("\nqw {bind}")
-    print("qw --add-bind")
+    print("    --add")
+    print("        Add bind")
+    print("    --list")
+    print("        Print list of binds")
+    print("    --delete")
+    print("        Delete bind")
     print("\n\nThanks!")
     sys.exit()
 
 bind = data_qview.bind
 command = data_qview.command
 
-if ("--add-bind" in sys.argv):
-    bind.append(input("bind: "))
-    command.append(input("command: "))
+to_del = ""
+
+if ("--list" in sys.argv):
+    print("{0:10s} {1:10s}".format("Bind", "Command"))
+    for i in range(len(command)):
+        print("{0:10s} {1:10s}".format(bind[i], command[i]))
+if ("--delete" in sys.argv):
+    to_del = " ".join(sys.argv).split("--delete ")[len(to_del)-1]
+    print(to_del)
+    for i in range(len(bind)):
+        if to_del==bind[i]:
+            print("\"{0}\" bind deleted.".format(bind[i]))
+            bind.pop(i)
+            command.pop(i)
+            break
+    os.system('echo "bind = %s" > /usr/bin/data_qview.py' % (str(bind)))
+    os.system('echo "command = %s" >> /usr/bin/data_qview.py' % (str(command)))
+
+
+if ("--add" in sys.argv):
+    bind.append(input("Bind: "))
+    command.append(input("Command: "))
     os.system('echo "bind = %s" > /usr/bin/data_qview.py' % (str(bind)))
     os.system('echo "command = %s" >> /usr/bin/data_qview.py' % (str(command)))
 
 u = " "
+
 for i in range(len(command)):
-    if ("%s" in command[i]):
-        if sys.argv[len(sys.argv)-1]!=bind[i]:
-            command[i] = command[i] % (str(sys.argv[len(sys.argv)-1]))
-        else:
-            command[i] = command[i] % u
     if (bind[i] in u.join(sys.argv)):
         os.system(command[i])
